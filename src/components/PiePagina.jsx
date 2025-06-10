560012e600c990f99764cab7d3dc2885a15dd180
DesireeCG <kami3haru@gmail.com>
2025-06-09 21:28:45 -0600

    pie3

--- src/components/PiePagina.jsx
+++ src/components/PiePagina.jsx
@@ -4,7 +4,7 @@ import logoipn from "../img/logo-ipn-horizontal.svg";
 
 export function PiePagina() {
   return (
-    <Box bg="blue.100" px={10} py={6} mt={10}>
+    <Box bg="blue.100" px={'28'} py={6} mt={10}>
       <Flex
         direction={{ base: "column", md: "row" }}
         justify="space-between"
@@ -14,13 +14,19 @@ export function PiePagina() {
       >
         
 
-        {/* Columna 2 */}
+        {/* Columna 1 */}
         <Stack spacing={2}>
           <Text fontWeight="bold" fontSize="sm">Contacto</Text>
           <Text fontSize="sm">Dr. Emmanuel Cabañas García</Text>
           <Text fontSize="sm">ecabanasg@ipn.mx</Text>
           <Text fontSize="sm">Dr. Mario César Ordóñez Gutiérrez</Text>
           <Text fontSize="sm">mordonezg@ipn.mx</Text>
+          
+        </Stack>
+
+        {/* Columna 2 */}
+        <Stack spacing={2} >
+          <Text fontWeight="bold" fontSize="sm">Desarrolladores</Text>
           <Text fontSize="sm">Axel Giovanni Ojeda Hernandez</Text>
           <Text fontSize="sm">aojedah1800@alumno.ipn.mx</Text>
           <Text fontSize="sm">Desireé Castañeda García</Text>
@@ -28,14 +34,6 @@ export function PiePagina() {
         </Stack>
 
         {/* Columna 3 */}
-        <Stack spacing={2} maxW="250px">
-          <Text fontWeight="bold" fontSize="sm">Acerca de</Text>
-          <Text fontSize="sm">
-             Este proyecto no tiene fines de lucro. Está desarrollado con propósitos educativos y de investigación, dirigido principalmente a estudiantes, docentes e investigadores interesados en el análisis de compuestos químicos mediante espectrometría de masas. Toda la información presentada se obtiene de fuentes públicas y se utiliza exclusivamente con fines académicos.
-          </Text>
-        </Stack>
-
-        {/* Columna 4 */}
         <Stack spacing={2}>
           <Text fontWeight="bold" fontSize="sm">Bases de Datos Utilizadas en el Proyecto</Text>
           <Link href="https://massbank.eu/MassBank/Search" fontSize="sm">MaskBank</Link>
@@ -44,10 +42,9 @@ export function PiePagina() {
         </Stack>
       </Flex>
 
-      <Divider my={4} />
+      <Divider my={3} />
 
-      <Image src={logoipn} alt="Logo IPN" w="150px" mt={4} mx="auto" />
-      
+            
       <Text fontSize="xs" textAlign="center" color="gray.600">
         © {new Date().getFullYear()} Todos los derechos reservados.
       </Text>
